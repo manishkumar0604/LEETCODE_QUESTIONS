b26f829bc232711ea9dbd6dfb67bdb758e2af729
manishkumar0604 <manishkrmfp2018@gmail.com>
2026-03-17 21:25:16 +0530

    Clarify sorting order in largestPerimeter function
    Added comment to clarify sorting order.

--- 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.cpp
+++ 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int largestPerimeter(vector<int>& nums) {
-        sort(nums.begin(),nums.end(),greater<int>());
+        sort(nums.begin(),nums.end(),greater<int>());  //reverse sort
         for(int i=0;i<nums.size()-2;i++)
         {
             if(nums[i+1]+nums[i+2] > nums[i])
