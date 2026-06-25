bd7a74af0c4df2dc2b486f83008a9a5c4a906830
Manish Kumar <manishkrmfp2018@gmail.com>
2026-06-25 23:52:52 +0530

    Time: 2499 ms (6.53%), Space: 494.1 MB (5.23%) - LeetHub

---
+++ 3737-count-subarrays-with-majority-element-i/3737-count-subarrays-with-majority-element-i.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int countMajoritySubarrays(vector<int>& nums, int target) {
+        int n = nums.size();
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            unordered_map<int, int> freq;
+
+            for (int j = i; j < n; j++) {
+                freq[nums[j]]++;
+
+                int len = j - i + 1;
+
+                if (freq[target] > len / 2) {
+                    ans++;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
